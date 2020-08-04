d004093de93442e5998b382f18ed8d44550bdda7
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-04 10:36:20 +0000

    検証

--- app/uploaders/icon_uploader.rb
+++ app/uploaders/icon_uploader.rb
@@ -17,8 +17,8 @@ class IconUploader < CarrierWave::Uploader::Base
    def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
   #    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #   "/images/" + [version_name, "default.png"].compact.join('_')
     "default.png"
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
    end
 
   # Process files as they are uploaded:
--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,10 @@ module CloneWithMailTask
     config.generators do |g|
      g.assets false
      g.helper false
+    config.i18n.default_locale = :ja
    end
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

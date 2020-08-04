8050bdbf505469327d060f290d1d24857fd400ac
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-04 00:12:13 +0000

    ユーザー詳細画面の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,34 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to
+      redirect_to user_path(@user.id)
     else
       render :new
     end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+
   end
   private
   def user_params
     params.require(:user).permit(:name, :email, :user_image, :password,
                                                              :password_confirmation)
+  end
+  def set_user
+    @user = User.find(params[:id])
   end
 end

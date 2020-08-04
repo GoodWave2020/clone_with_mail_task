c509d4962f65676f1967c90b8fe4724d4c3efa08
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-04 00:34:01 +0000

    ログインフォームの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  resources :sessions, only: [:new, :create, :destroy]
   resources :pictures
   resources :users, only: [:new, :create, :edit, :update, :show]
 end

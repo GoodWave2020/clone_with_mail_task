8622c1635fad64e21fca8f24d9ed7460c6f3b97b
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-06 03:45:47 +0000

    ログイン画面のcss, htmlを編集

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,4 +1,3 @@
-I18n.load_path += Dir[Rails.root.join('config', 'initializers', 'locales', '*.yml')]
-#↑これです
+I18n.load_path += Dir[Rails.root.join('config', 'initializers', 'locales', '**', '*.{rb,yml}')]
 I18n.available_locales = :ja
 I18n.default_locale = :ja

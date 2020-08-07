4fa442423c93367bbc2a60c655858ddcc2fdec3c
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-07 08:14:27 +0000

    アカウント作成後のリダイレクトを変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :favorites]
   before_action :current_user
-  
+
   def new
     @user = User.new
   end
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
+      redirect_to new_session_path
     else
       render :new
     end

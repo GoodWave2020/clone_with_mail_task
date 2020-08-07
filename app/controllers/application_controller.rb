bdb8d31628678457e37747bb2edc7d346231e915
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-07 11:58:45 +0000

    辞書を追加、各ページを辞書機能で日本語表示に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,21 +3,21 @@ class ApplicationController < ActionController::Base
   include ApplicationHelper
   def authenticate_user
     if @current_user == nil
-      flash[:notice] = t('notice.login_needed')
+      flash[:notice] = I18n.t(:login_needed)
       redirect_to new_session_path
     end
   end
 
   def check_user_for_user
     if @current_user != @user
-      flash[:notice] = "you can't destroy or edit other user's profile"
+      flash[:notice] = I18n.t("you_can't_edit_other_user's_profile")
       redirect_to pictures_path
     end
   end
 
   def check_user_for_picture
     if @current_user != @picture.user
-      flash[:notice] = "you can't destroy or edit other user's pictures"
+      flash[:notice] = I18n.t("you_can't_destroy_or_edit_other_user's_pictures")
       redirect_to pictures_path
     end
   end
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,3 +1,2 @@
-#I18n.load_path += Dir[Rails.root.join('config', 'initializers', 'locales', '**', '*.{rb,yml}')]
 I18n.available_locales = :ja
 I18n.default_locale = :ja
